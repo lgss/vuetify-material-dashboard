38a59fdb82952fff653f39fc812c394b83ad24ca
Dan Blundell <hello@danblundell.com>
2020-01-14 09:09:23 +0000

    remove parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+  parserOptions: {}
 }
